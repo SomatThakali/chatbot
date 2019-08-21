75a369887c9f72f65e95c84fb9bff0b4a5ff732e
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-08-21 17:14:21 -0400

    Updated the version

--- build_training_model.js
+++ build_training_model.js
@@ -6,11 +6,15 @@ const twilio = require("twilio");
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const PORT = 3000;
+// const PORT = 3000;
 
 const client = new twilio(accountSid, authToken);
 // UGf8ef5b4055f7159dd80b7d685ea7fdac
 client.autopilot
   .assistants("UA67ad459451ae8ab1cbb7ff598da1be5d")
-  .modelBuilds.create({ uniqueName: "v0.1" })
+  .modelBuilds.create({ uniqueName: "v0.2" })
   .then(model_build => console.log(model_build.sid));
+
+// app.listen(PORT, function() {
+//   console.log("App listening on http://localhost:" + PORT);
+// });
